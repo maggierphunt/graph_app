79fba0979f4a5bbe861fb47e457e4b0386a6466a
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-06 19:30:37 +0000

    Create landingpage.py

---
+++ landingpage.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask("Myapp") #making an app
+@app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def hello():
+        return "Welcome!"
+
+@app.route("/<name>")
+def hello_someone(name):
+        return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+        form_data = request.form
+        print (form_data["email"])
+        return "All OK"
+
+app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
