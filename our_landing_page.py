900e08cae1aa87985fe4824f083f983a930fb66b
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-18 13:22:39 +0000

    landing page fix

--- our_landing_page.py
+++ our_landing_page.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask("our_app") #making an app
 @app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+<<<<<<< Updated upstream
 def home():
         return render_template("our_landing_page.html")
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
+=======
+def main():
+        return ("Welcome to our hashtag tracker!")
+
+app.run(debug=True) #runs the app the debug part - unlocks debugging feature.
+>>>>>>> Stashed changes
