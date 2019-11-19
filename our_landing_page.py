fc9b338882c522a82fc2de6e62a0ef5a94f2b398
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-19 16:58:22 +0000

    layout adjustments

--- our_landing_page.py
+++ our_landing_page.py
@@ -5,7 +5,6 @@ app = Flask("our_app") #making an app
 def main():
         return render_template("our_landing_page.html")
         import matplotlib.pyplot as plt
-
         year = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
         pop_twitter_use = [44.91, 58.09, 78.07, 107.7, 138.5, 170.6, 7, 8, 9, 10, 2, 3, 4, 0]
         plt.plot(year, pop_twitter_use, color='blue')
