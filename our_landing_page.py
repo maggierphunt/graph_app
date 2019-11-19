f549b910aa817565805ae62e6698cb2fd225ce11
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-19 12:15:07 +0000

    changes

--- our_landing_page.py
+++ our_landing_page.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask("our_app") #making an app
 @app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-<<<<<<< Updated upstream
+
 def main():
         return render_template("our_landing_page.html")
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
