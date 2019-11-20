eb204d9d6d0d7de05e7a258418c27d75a6386658
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-20 12:18:17 +0000

    Update our_landing_page.py

--- our_landing_page.py
+++ our_landing_page.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request
 app = Flask("our_app") #making an app
-@app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-
+@app.route("/", methods=["GET"])    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
 def our_landing_page():
+        form_data = request.form.get
         return render_template("our_landing_page.html")
-form_data = request.form.get
-import matplotlib.pyplot as plt
-x_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-y_value = ["value_1","value_2", "value_3", "value_4", "value_5", "value_6", "value_7", "value_8", "value_9", "value_10", "value_11", "value_12"]
-plt.plot(x_value, y_value, color='pink')
-plt.xlabel("x_axis")
-plt.ylabel("y_axis")
-plt.title("form_title")
-plt.show()
-s.plot.bar()
-fig.savefig('my_graph.png')
+        import matplotlib.pyplot as plt
+        x_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        y_value = ["value_1","value_2", "value_3", "value_4", "value_5", "value_6", "value_7", "value_8", "value_9", "value_10", "value_11", "value_12"]
+        plt.plot(x_value, y_value, color='pink')
+        plt.xlabel("x_axis")
+        plt.ylabel("y_axis")
+        plt.title("form_title")
+        plt.show()
+        s.plot.bar()
+        fig.savefig('my_graph.png')
+
 
 @app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
 
 def about():
         return render_template("about.html")
 
-@app.route("/graph_page", methods=["GET"]) 
+@app.route("/graph_page") 
 def graph_page():
        return render_template("graph_page.html")
 
