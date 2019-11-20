c3556635671a28d8633d93024c41bf61697d3878
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-20 11:31:59 +0000

    Update our_landing_page.py

--- our_landing_page.py
+++ our_landing_page.py
@@ -13,17 +13,14 @@ def about():
 @app.route("/graph_page", methods=["GET"]) 
 def graph_page():
        return render_template("graph_page.html")
-        form_data = request.form
-        import matplotlib.pyplot as plt
-        time_passing = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        graph_line = ["value_1","value_2", "value_3", "value_4", "value_5", "value_6", "value_7", "value_8", "value_9", "value_10", "value_11", "value_12"]
-        plt.plot(time_passing, graph_line, color='pink')
-        plt.xlabel("x_axis")
-        plt.ylabel("y_axis")
-        plt.title("form_title")
-        plt.show()
-        imgplot = plt.imshow(img)
-        plt.imshow(sample_image) 
-        plt.show() 
+form_data = request.form.get
+import matplotlib.pyplot as plt
+x_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+y_value = ["value_1","value_2", "value_3", "value_4", "value_5", "value_6", "value_7", "value_8", "value_9", "value_10", "value_11", "value_12"]
+plt.plot(x_value, y_value, color='pink')
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+plt.title("form_title")
+plt.show()
 
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
