45d1655c4ea2e5f9fbc2f83afcb4c84996549b3e
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-11-21 00:26:42 +0000

    Update appscript.js

--- static/js/appscript.js
+++ static/js/appscript.js
@@ -1,78 +1,51 @@
 function validateForm() {
     var x = document.forms["Form"]["value_1"].value;
-    if (x = "") {
-      alert("Please provide 12 values.");
-      return false;}
     if (x == "number") {
        alert("Please only enter numerical values.");
        return false;}
     var x = document.forms["Form"]["value_2"].value;
-    if (x == "") {
-      alert("Please provide 12 values.");
-      return false;}
     if (x == "number") {
        alert("Please only enter numerical values.");
        return false;}
     var x = document.forms["Form"]["value_3"].value;
-    if (x == "") {
-      alert("Please provide 12 values.");
-      return false;}
     if (x == "number") {
        alert("Please only enter numerical values.");
        return false;}
     var x = document.forms["Form"]["value_4"].value;
-    if (x == "") {
-      alert("Please provide 12 values.");
-      return false;}
     if (x == "number") {
        alert("Please only enter numerical values.");
        return false;}
        var x = document.forms["Form"]["value_5"].value;
-       if (x == "") {
-         alert("Please provide 12 values.");
-         return false;}
        if (x == "number") {
           alert("Please only enter numerical values.");
           return false;}
        var x = document.forms["Form"]["value_6"].value;
-       if (x == "") {
-         alert("Please provide 12 values.");
-         return false;}
        if (x == "number") {
           alert("Please only enter numerical values.");
           return false;}
        var x = document.forms["Form"]["value_7"].value;
-       if (x == "") {
-         alert("Please provide 12 values.");
-         return false;}
        if (x == "number") {
           alert("Please only enter numerical values.");
           return false;}
        var x = document.forms["Form"]["value_8"].value;
-       if (x == "") {
-         alert("Please provide 12 values.");
-         return false;}
        if (x == "number") {
           alert("Please only enter numerical values.");
           return false;}
-
         var x = document.forms["Form"]["value_9"].value;
-        if (x == "") {
-            alert("Please provide 12 values.");
-            return false;}
-        
+        if (x == "number") {
+            alert("Please only enter numerical values.");
+            return false;}        
         var x = document.forms["Form"]["value_10"].value;
-        if (x == "") {
-            alert("Please provide 12 values.");
+        if (x == "number") {
+            alert("Please only enter numerical values.");
             return false;}
         
         var x = document.forms["Form"]["value_11"].value;
-        if (x == "") {
-            alert("Please provide 12 values.");
+        if (x == "number") {
+            alert("Please only enter numerical values.");
             return false;}
         
         var x = document.forms["Form"]["value_12"].value;
-        if (x == "") {
-            alert("Please provide 12 values.");
+        if (x == "number") {
+            alert("Please only enter numerical values.");
             return false;}
-    return true;}
